bdeb6453df02fcad8bbbe36fb853d92315411774
puranmgr <puranmgr@gmail.com>
2019-03-26 00:08:05 +0000

    added rank component

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Rank = () => {
+
+    return (
+        <div>
+            <div className='f3'>
+                {'Puran, your current rank is...'}
+            </div>
+            <div className='f1'>
+                {'#9'}
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
