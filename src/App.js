670d20e3be91f67924023f82f2c73d93892fd9a7
puranmgr <puranmgr@gmail.com>
2019-03-26 15:21:14 +0000

    imported clarifai api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank'
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: 'f8c35ba463d94cdbb39b2f75849f4e6f'
+});
 
 const particlesOption = {
   particles: {
@@ -30,8 +35,21 @@ class App extends Component {
   onInputChange = (event) => {
     console.log(event.target.value);
   }
+
   onButtonSubmit = () => {
     console.log('click');
+    app.models
+      .predict("a403429f2ddf4b49b307e318f00e528b",
+        "https://samples.clarifai.com/face-det.jpg")
+      .then(
+        function (response) {
+          // do something with response
+          console.log(response);
+        },
+        function (err) {
+          // there was an error
+        }
+      );
   }
   render() {
     return (
