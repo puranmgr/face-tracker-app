4d7df6802aeff6331a3bbfb177612ebf6efa7b4f
puranmgr <puranmgr@gmail.com>
2019-03-26 13:04:23 +0000

    added initial func for input & detect button

--- src/App.js
+++ src/App.js
@@ -20,6 +20,19 @@ const particlesOption = {
   }
 }
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+  onButtonSubmit = () => {
+    console.log('click');
+  }
   render() {
     return (
       <div className="App">
@@ -29,7 +42,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
         {/*<FaceTracker />*/}
       </div>
     );
