7dacf043c3483d21a81c9e36f5c938e282d84540
puranmgr <puranmgr@gmail.com>
2019-03-27 18:37:25 +0000

    implemented calculateFaceLocation function

--- src/App.js
+++ src/App.js
@@ -36,8 +36,23 @@ class App extends Component {
   }
 
   calculateFaceLocation = (data) => {
-
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
   }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({ box: box });
+  }
+
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
   }
@@ -48,7 +63,7 @@ class App extends Component {
       .predict(
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
-      .then((response) => this.calculateFaceLocation(response))
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
       .catch(err => console.log(err));
 
   }
@@ -65,7 +80,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        <FaceTracker imageUrl={this.state.imageUrl} />
+        <FaceTracker box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
