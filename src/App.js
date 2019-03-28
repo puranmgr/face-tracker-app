92eda46778442f4c96b7fe70981dac0f8a8a660a
puranmgr <puranmgr@gmail.com>
2019-03-28 13:06:37 +0000

    fixed signing in, sign out and register route

--- src/App.js
+++ src/App.js
@@ -34,7 +34,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -72,6 +73,11 @@ class App extends Component {
   }
 
   onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({ isSignedIn: false })
+    } else if (route === 'home') {
+      this.setState({ isSignedIn: true })
+    }
     this.setState({ route: route });
   }
   render() {
@@ -80,7 +86,7 @@ class App extends Component {
         <Particles className='particles'
           params={particlesOption}
         />
-        <Navigation onRouteChange={this.onRouteChange} />
+        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange} />
         {this.state.route === 'home'
           ?
           <div>
