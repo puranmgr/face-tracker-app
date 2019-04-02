ef0261cd143272ae80c49166142a7cfc7fbf69ac
puranmgr <puranmgr@gmail.com>
2019-04-02 17:43:28 +0100

    register route change done

--- src/App.js
+++ src/App.js
@@ -35,8 +35,27 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+      }
+    })
   }
 
   calculateFaceLocation = (data) => {
@@ -100,7 +119,7 @@ class App extends Component {
           : (
             this.state.route === 'signin'
               ? < Signin onRouteChange={this.onRouteChange} />
-              : < Register onRouteChange={this.onRouteChange} />
+              : < Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
           )
         }
       </div>
